45bc635a11160098d4dcba9dbb449d6d93ceb1ac
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-01-09 09:23:48 -0300

    Usando espacios en lugar de tabs

--- 401_elementos3-upla/test.hs
+++ 401_elementos3-upla/test.hs
@@ -6,12 +6,12 @@ describe "posicion3-upla" $ do
     it "el primer elemento de la tupla (3,5/0,0.3) es 3" $ do
       fst3 (3,5/0,0.3)  `shouldBe` 3
 
-  describe "snd3" $ do 
+  describe "snd3" $ do
     it "el segundo elemento de la tupla (5,"Hola",4) es "Hola"" $ do
       snd3 (5,"Hola",4) `shouldBe` "Hola"
-	
-	it "el segundo elemento de la tupla (3/0,4,6) es 4" $ do
-	  snd3 (3/0,4,6) `shouldBe` 4
+
+  it "el segundo elemento de la tupla (3/0,4,6) es 4" $ do
+    snd3 (3/0,4,6) `shouldBe` 4
 
   describe "trd3" $ do
     it "el tercer elemento de la tupla (6,9,3) es 3" $ do
